9c4cfda683598c491962666bc8db11c355ec9f0a
Rukshan Dissanayake <rukshanmali@gmail.com>
2025-05-10 23:01:52 +0300

    Use bitwise exor ^ to swap numbers

--- src/com/task2/NumberSwapping.java
+++ src/com/task2/NumberSwapping.java
@@ -15,11 +15,21 @@ public class NumberSwapping {
 		System.out.println(a + " and " + b);
 		
 		
-		// a = 1  and b = 25
+		// a = 16  and b = 25
 		// using mathematical operators to swap
 		a = a + b; // 16+25 = 41
 		b = a - b; // 41-25 = 16
 		a = a - b; // 41-16 = 25
+		
+		System.out.println(a + " and " + b);
+		
+		
+		// using bitwise operators (^) exor to swap numbers
+		// a= 25 and b = 16
+		
+		a = a^b; // a = 41
+		b = a^b; // b = 25
+		a = a^b; // a = 16
 		
 		System.out.println(a + " and " + b);
 		
